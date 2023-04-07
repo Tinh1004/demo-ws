b1816dfcc110da0ed94752bc953a7fe1f2220409
Tinh1004 <1004tinh@gmail.com>
2023-04-07 19:25:21 +0700

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Server } = require("ws");
 const PORT = process.env.PORT || 3000; //port for https
 
 const server = express()
-  .use((req, res) => res.send("Hi there"))
+  .use("/", (req, res) => res.send("Hi there"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
