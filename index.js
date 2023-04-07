db26405587e8954dfb6f3c9b4f478e1a090e6cc3
Tinh1004 <1004tinh@gmail.com>
2023-04-07 22:18:05 +0700

    update socket.io

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
+// const express = require("express");
+// const { Server } = require("ws");
+
+// const PORT = process.env.PORT || 3000; //port for https
+
+// const server = express()
+//   .use("/", (req, res) => {
+//     return res.send("Hello World!!!");
+//   })
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// const wss = new Server({ server });
+
+// wss.on("connection", function (ws, req) {
+//   var userID = req.url.substr(1); //get userid from URL/userid
+//   console.log("User connected:", userID);
+//   ws.on("message", (message) => {
+//     // If there is any message
+//     var datastring = message.toString();
+//     console.log("Print:", datastring);
+//     if (datastring.charAt(0) == "{") {
+//       datastring = datastring.replace(/\'/g, '"');
+//       var data = JSON.parse(datastring);
+//       console.log("data:", data);
+//       console.log("data.cmd:", data.cmd);
+//       ws.send(data.cmd);
+//     }
+//   });
+// });
+
 const express = require("express");
-const { Server } = require("ws");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 3000; //port for https
+app.use(cors());
 
-const server = express()
-  .use("/", (req, res) => {
-    return res.send("Hello World!!!");
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = http.createServer(app);
 
-const wss = new Server({ server });
+const io = new Server(server);
 
-wss.on("connection", function (ws, req) {
-  var userID = req.url.substr(1); //get userid from URL/userid
-  console.log("User connected:", userID);
-  ws.on("message", (message) => {
-    // If there is any message
-    var datastring = message.toString();
-    console.log("Print:", datastring);
-    if (datastring.charAt(0) == "{") {
-      datastring = datastring.replace(/\'/g, '"');
-      var data = JSON.parse(datastring);
-      console.log("data:", data);
-      ws.send(data.cmd);
-    }
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket}`);
+
+  socket.on("event", (data) => {
+    console.log("event", data);
+    // socket.join(data);
   });
+
+  socket.on("fromServer", (data) => {
+    console.log("fromServer");
+    // socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+app.get("/", (req, res) => {
+  io.emit("event", "Long occho");
+  return res.send("Hello World!");
+});
+
+server.listen(3000, () => {
+  console.log("SERVER IS RUNNING");
 });
