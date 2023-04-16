d25373f3546e6cfcfb52cf50772f7c2beb533c95
Tinh1004 <1004tinh@gmail.com>
2023-04-17 01:39:41 +0700

    update ws io to emit

--- module_sokect.js
+++ module_sokect.js
@@ -5,11 +5,26 @@ const msgErrorNotFound = [
   "Xin lỗi, tôi không hiểu bạn nói gì!",
   "Bạn có thể nhắc lại điều đó cho tôi được không!",
   "Bạn có thể nói chi tiết cho tôi được không ạ!",
-  "",
+  "Xin lỗi tôi là 1 con bot chưa có hoàn thiện nên không thể hiểu hết những câu hỏi của bạn.",
 ];
+const errorMsg = "Xảy ra lỗi trong server!";
+var errorNotFoundCount = 0;
+var errorNotFoundMessage =
+  "Tôi không hiểu bạn nói gì.\ntôi có 1 vài ý kiến:\n1. Menu\n2. chức năng khác...";
+
+var users = [];
 
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket}`);
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("joinUser", (user) => {
+    console.log("user", user);
+    users.push({
+      id: user._id,
+      socketId: socket.id,
+    });
+  });
+
   socket.on("event", (data) => {
     console.log("event", data);
     // socket.join(data);
@@ -25,23 +40,64 @@ io.on("connection", (socket) => {
     io.emit("focus", data);
   });
 
-  socket.on("sendChat", (data) => {
-    const message = JSON.parse(data);
-    var msgAnswer = chatbotService.getAnswer(message.message);
+  socket.on("sendChat", async (data) => {
+    console.log("data", data);
+    try {
+      const message = data;
+      var msgAnswer = await chatbotService.getAnswer(message.message);
+      console.log("sendChat", data);
+      console.log("msg", msgAnswer);
 
-    console.log("sendChat", data);
-    console.log("msg", msgAnswer);
+      const findUser = users.find((e, i) => (e.id = data.userId));
+      console.log("findUser", findUser);
 
-    if (!msgAnswer) {
-      const random = Math.floor(Math.random() * months.length);
-      msgAnswer = msgErrorNotFound[random];
+      if (!msgAnswer) {
+        errorNotFoundCount++;
+
+        if (errorNotFoundCount == 3) {
+          msgAnswer = errorNotFoundMessage;
+          errorNotFoundCount = 0;
+        } else {
+          const random = Math.floor(Math.random() * msgErrorNotFound.length);
+          console.log(
+            "random",
+            random,
+            msgErrorNotFound.length,
+            msgErrorNotFound.length == random
+          );
+
+          msgAnswer = msgErrorNotFound[random];
+        }
+      } else {
+        errorNotFoundCount = 0;
+      }
+      const jsonObject = {
+        user: "Bot",
+        userId: "1",
+        message: msgAnswer,
+      };
+
+      io.to(`${findUser.socketId}`).emit("sendChat", jsonObject);
+    } catch (error) {
+      console.log("error");
+      // const jsonObject = {
+      //   user: "Bot",
+      //   userId: "1",
+      //   message: errorMsg,
+      // };
+
+      // io.emit("sendChat", jsonObject);
     }
-    const jsonObject = {
-      user: "Bot",
-      message: msgAnswer,
-    };
-
-    io.emit("sendChat", jsonObject);
+  });
+  socket.on("disconnect", () => {
+    console.log("Khách hàng đã ngắt kết nối:", socket.id);
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].id == id) {
+        users.splice(i, 1);
+        break;
+      }
+    }
+    console.log(users);
   });
 });
 
--- services/chatbot.service.js
+++ services/chatbot.service.js
@@ -2,20 +2,19 @@ const Question = require("../models/Question.model");
 
 const chatbotService = {
   getAnswer: async (question) => {
-    await Question.find({
-      question: { $regex: question, $options: "i" },
-    })
-      .then((value) => {
-        console.log("value", value);
-        if (value.length > 0) {
-          return value[0].question;
-        } else {
-          return "";
-        }
-      })
-      .catch((e) => {
-        return "";
+    try {
+      const questions = await Question.find({
+        question: { $regex: question, $options: "i" },
       });
+      console.log("value", questions);
+      if (questions.length > 0) {
+        return questions[0].answer;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      return null;
+    }
   },
 };
 
