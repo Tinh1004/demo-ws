5023df56fbcfea6e26a69a8dddabbf5404352245
Tinh1004 <1004tinh@gmail.com>
2023-04-27 23:20:16 +0700

    fix cors

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ app.use(cookieParser());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Methods", "Content-Type, x-requested-with, Origin, Authorization");
   next();
 });
 
